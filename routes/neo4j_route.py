eca07ede2a594dc1a5ef6efb09962931773895c3
ZYH <1900909586@qq.com>
2026-01-14 18:56:26 +0800

    fix: update neo4j_route

--- routes/neo4j_route.py
+++ routes/neo4j_route.py
@@ -174,6 +174,7 @@ def get_software_techniques_tactics_route():
 
     # 执行查询
     data = get_software_techniques_tactics(software_id)
+    
 
     if data['tactics_count'] == 0:
         return jsonify({'code': '200', 'message': '未找到相关战术和技术信息', 'data': data})
